84a3f0acd5f24cb10d63bb655198371daae8c48d
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2025-06-19 12:39:48 +0700

    Create Challenge #1.2.py

---
+++ Challenge #1.2.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+'''
+Ask the user to input three numbers and calculate their average.
+'''
+
+# Simple Average Calculation
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+average = (num1 + num2 + num3) / 3
+print("Average: ", average)
