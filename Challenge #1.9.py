623d978c5c7aeba7b0d05e6b4fcc1f1940081e4e
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2025-06-19 12:51:04 +0700

    Create Challenge #1.9.py

---
+++ Challenge #1.9.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+'''
+Ask the user to input a distance in kilometers and convert it to meters.
+'''
+
+# Convert Kilometers to Meters
+kilometers = float(input("Enter distance in kilometers: "))
+meters = kilometers * 1000
+
+print(kilometers, "kilometers is equal to", meters, "meters.")
