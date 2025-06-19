14e6533e116831dee5e1ba01e0bb1f75353851cc
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2025-06-19 12:52:06 +0700

    Create Challenge #1.10.py

---
+++ Challenge #1.10.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+'''
+Ask the user to input a length in inches and convert it to centimeters.
+'''
+
+# Convert Inches to Centimeters
+inches = float(input("Enter length in inches: "))
+centimeters = inches * 2.54
+
+print(inches, "inches is equal to", centimeters, "centimeters.")
