f9b5538ba8e9527b2aadf3fe79f593cd7a6dd040
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2025-06-19 12:42:48 +0700

    Create Challenge #1.4.py

---
+++ Challenge #1.4.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+'''
+Ask the user to input a number and print its square.
+'''
+
+# Square of a Number
+number = float(input("Enter a number: "))
+square = number * number
+
+print("The square is: ",square)
